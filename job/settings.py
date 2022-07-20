9f8dd4bdb510640cabf389eaf4f137cfa393f78b
rahberabbas <amanabidi1234@gmail.com>
2022-07-20 17:00:07 +0530

    first commit

--- job/settings.py
+++ job/settings.py
@@ -168,12 +168,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-
-# STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_ROOT = '/home/ubuntu/myproject/SomhakoRevamp/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_old")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
