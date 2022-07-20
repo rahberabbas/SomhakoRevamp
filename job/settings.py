170a2d46c030b19b91875a3eca97b3df0ae52890
rahberabbas <amanabidi1234@gmail.com>
2022-07-20 16:54:04 +0530

    first commit

--- job/settings.py
+++ job/settings.py
@@ -172,7 +172,7 @@ STATIC_URL = '/static/'
 #     os.path.join(BASE_DIR, "static"),
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
